1105742f1d9d8eeeafd379fe525801273d480740
coppercele <mokeuhi@hotmail.com>
2023-08-15 18:30:19 +0900

    modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -13,25 +13,18 @@ Serial_CAN can;
 #define PID_VEHICLE_SPEED 0x0D
 #define PID_COOLANT_TEMP 0x05
 
-#if STANDARD_CAN_11BIT
-#define CAN_ID_PID 0x7DF
-#else
-#define CAN_ID_PID 0x18db33f1
-#endif
-
 void sendPid(unsigned char __pid) {
+  M5.Lcd.println("sendPid: start");
   unsigned char tmp[8] = {0x02, 0x01, __pid, 0, 0, 0, 0, 0};
 
-#if STANDARD_CAN_11BIT
-  can.send(CAN_ID_PID, 0, 0, 8, tmp); // SEND TO ID:0X55
-#else
-  can.send(CAN_ID_PID, 1, 0, 8, tmp); // SEND TO ID:0X55
-#endif
+  // can.send(0x7DF, 0, 0, 8, tmp); // SEND TO ID:0X55
+  can.send(0x18db33f1, 1, 0, 8, tmp); // SEND TO ID:0X55
+  M5.Lcd.println("sendPid: end");
 }
+
 bool getRPM(int *r) {
   M5.Lcd.println("Start getRPM: ");
   sendPid(PID_ENGIN_PRM);
-  M5.Lcd.println("sendPid: ");
   unsigned long __timeout = millis();
 
   while (millis() - __timeout < 1000) {
@@ -52,31 +45,57 @@ bool getRPM(int *r) {
 }
 
 void setup() {
-  M5.begin();
+  M5.begin(true, false, true, false);
   M5.Lcd.fillScreen(BLACK);
-  M5.Lcd.setTextColor(GREEN);
+  M5.Lcd.setTextColor(GREEN, BLACK);
   // 文字描画
   M5.Lcd.setTextSize(2);
   M5.Lcd.setCursor(0, 0);
-  M5.Lcd.println("CAN setup");
-  Serial1.begin(9600, SERIAL_8N1, can_rx, can_tx); // RX,TX
+  // Serial1.begin(115200, SERIAL_8N1, can_rx, can_tx); // RX,TX
 
-  can.begin(Serial1, 9600);
-  // Serial1.begin(500000, SERIAL_8N1, can_rx, can_tx); // RX,TX
-
-  // can.begin(Serial1, 500000);
-  M5.Lcd.println("end");
+  // can.begin(Serial1, 115200);
 }
 
+bool isStart = false;
+unsigned long baud = 9600;
+
 void loop() {
-  M5.Lcd.println("Start: ");
-  int __rpm = 0;
-  int ret = getRPM(&__rpm);
-  if (ret) {
-    M5.Lcd.print("Engin Speed: ");
-    M5.Lcd.print(__rpm);
-    M5.Lcd.println(" rpm");
+  M5.update();
+  M5.Lcd.setCursor(M5.Lcd.width() * 0.7, 0);
+  M5.Lcd.printf("%6ld", baud);
+
+  if (M5.BtnA.wasReleased()) {
+    if (baud == 9600) {
+      baud = 115200;
+    }
+    else if (baud == 115200) {
+      baud = 500000;
+    }
+    else if (baud == 500000) {
+      baud = 9600;
+    }
   }
-  delay(500);
+  if (M5.BtnB.wasReleased()) {
+    isStart = true;
+  }
+
+  if (isStart) {
+    M5.Lcd.setCursor(0, 0);
+    M5.Lcd.println("CAN setup");
+    M5.Lcd.println("end");
+    Serial1.begin(baud, SERIAL_8N1, can_rx, can_tx); // RX,TX
+
+    can.begin(Serial1, baud);
+
+    M5.Lcd.println("Start: ");
+    int __rpm = 0;
+    int ret = getRPM(&__rpm);
+    if (ret) {
+      M5.Lcd.print("Engin Speed: ");
+      M5.Lcd.print(__rpm);
+      M5.Lcd.println(" rpm");
+    }
+    delay(500);
+  }
   delay(1);
 }
